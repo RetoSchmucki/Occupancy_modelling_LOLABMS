4f1aefdc7a61a7f90109c50e7b8276b77e8dff2d
RetoSchmucki <reto_schmucki@hotmail.com>
2015-06-13 13:23:50 +0200

    add 5 hours to the machine

--- sub.sh
+++ sub.sh
@@ -4,7 +4,7 @@ lieu1="$PWD"
   for i in `seq 1`
   do
     echo "iteration_${i}"
-    qsub  -l ct=40:00:00 -l vmem=16G -l fsize=3G -j y -o $lieu1/out.txt $lieu1/reto.sh ${i} $lieu1
+    qsub  -l ct=45:00:00 -l vmem=16G -l fsize=3G -j y -o $lieu1/out.txt $lieu1/reto.sh ${i} $lieu1
   done
 
 
