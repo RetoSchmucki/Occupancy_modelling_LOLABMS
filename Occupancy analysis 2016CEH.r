e41d076571ba46adfc72b86596fbab76b74b2d88
RetoSchmucki <reto.schmucki@mail.mcgill.ca>
2016-10-17 16:06:01 +0100

    initial commit for SEF analyses

---
+++ Occupancy analysis 2016CEH.r
@@ -0,0 +1,11 @@
+## =================================
+## get the occupancy model results
+## Date 17.10.2016
+## Author Reto Schmucki
+## =================================
+
+R
+
+load("C:/Users/RETOSCHM/OneDrive - Natural Environment Research Council/LOLA_BMS/estimate_all_AUGUST/countries_UNI_2006_sd_67/Anthocharis_cardamines/Anthocharis cardamines.Rdata")
+load("C:/Users/RETOSCHM/OneDrive - Natural Environment Research Council/LOLA_BMS/estimate_all_AUGUST/countries_UNI_2006_sd_67/Anthocharis_cardamines/results/jagsoutput2.Rdata")
+ls()
